1af307c68bb9e4991aeaa1721d39d824da632f13
Pedro <pedromattiello@gmail.com>
2026-01-08 12:48:53 -0300

    ci: test circle

--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -22,7 +22,7 @@ export class HealthService {
     const redisStatus = 'not-checked'
 
     return {
-      status: 'success',
+      status: 'success2',
       uptime,
       database: databaseStatus,
       timestamp,
