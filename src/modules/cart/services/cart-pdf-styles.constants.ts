397df1f156e771a600c2e4273b74eb2ae4434234
Pedro <pedromattiello@gmail.com>
2026-01-21 11:05:25 -0300

    test: ci

--- src/modules/cart/services/cart-pdf-styles.constants.ts
+++ src/modules/cart/services/cart-pdf-styles.constants.ts
@@ -360,3 +360,4 @@ export const DETAILED_PDF_STYLES = `
 
 
 
+
--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -22,7 +22,7 @@ export class HealthService {
     const redisStatus = 'not-checked'
 
     return {
-      status: 'success3',
+      status: 'success',
       uptime,
       database: databaseStatus,
       timestamp,
