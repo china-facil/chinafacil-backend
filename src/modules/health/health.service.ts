6621bf4d0368f2b1ce2532d840c4443c00a23ad5
Pedro <pedromattiello@gmail.com>
2026-02-03 15:25:55 -0300

    ci: test

--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -23,8 +23,8 @@ export class HealthService {
 
     return {
       uptime,
-      status: 'success',
       database: databaseStatus,
+      status: 'success',
       timestamp,
       redis: redisStatus,
     }
